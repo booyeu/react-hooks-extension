86749d7d60b87dbf0a717c687b0ef9910524fc9c
liulirui <admin@liulirui.com>
2023-09-26 22:15:13 +0800

    fix: useStateRef use function

--- src/useStateRef.ts
+++ src/useStateRef.ts
@@ -1,11 +1,10 @@
-import { useCallback, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export default function <T>(initialValue?: T) {
   const [state, setState] = useState(initialValue);
   const ref = useRef(initialValue);
-  const set = useCallback((val: T) => {
-    ref.current = val;
-    setState(val);
-  }, []);
-  return [state, set, ref] as const;
+  useEffect(() => {
+    ref.current = state;
+  }, [state]);
+  return [state, setState, ref] as const;
 }
